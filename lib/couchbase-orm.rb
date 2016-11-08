40ad4566b8ae84ff738d167d51a409cc68e7448f
Stephen von Takach <steve@cotag.me>
2016-11-08 15:35:34 +1100

    (conversions) provide boolean type helper

--- lib/couchbase-orm.rb
+++ lib/couchbase-orm.rb
@@ -9,6 +9,32 @@ module CouchbaseOrm
     autoload :Base,        'couchbase-orm/base'
 end
 
+# Provide Boolean conversion function
+# See: http://www.virtuouscode.com/2012/05/07/a-ruby-conversion-idiom/
+module Conversions
+    private
+
+    def Boolean(value)
+        case value
+        when String, Symbol
+            case value.to_s.strip.downcase
+            when 'true'
+                return true
+            when 'false'
+                return false
+            end
+        when Integer
+            return value != 0
+        when false, nil
+            return false
+        when true
+            return true
+        end
+
+        raise ArgumentError, "invalid value for Boolean(): \"#{value.inspect}\""
+    end
+end
+
 # If we are using Rails then we will include the Couchbase railtie.
 if defined?(Rails)
     require 'couchbase-orm/railtie'
